6ed8f8e13e565b38067a08467bf0e1c0a5aef3c3
IgorRasenkovKurs <154912805+IgorRasenkovKurs@users.noreply.github.com>
2024-05-27 21:10:27 +0300

    Add files via upload

---
+++ Alg_Hill/Alg_Hill/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Alg_Hill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(26, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(410, 22);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(26, 40);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(410, 22);
+            this.textBoxKey.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(443, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите текст для шифрования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите ключ";
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(26, 68);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(132, 23);
+            this.buttonDecrypt.TabIndex = 4;
+            this.buttonDecrypt.Text = "Шифрование";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(26, 97);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(132, 23);
+            this.buttonEncrypt.TabIndex = 5;
+            this.buttonEncrypt.Text = "Дешифрование";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(26, 127);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 16);
+            this.labelResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 207);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Hill_alg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Alg_Hill/Alg_Hill/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Alg_Hill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+
+            string inputText = textBoxInput.Text;
+            string key = textBoxKey.Text;
+
+            string encryptedText = HillCipher.Encrypt(inputText, key);
+            labelResult.Text = "Зашифрованный текст: " + encryptedText;
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            string inputText = textBoxInput.Text;
+            string key = textBoxKey.Text;
+
+            string decryptedText = HillCipher.Decrypt(inputText, key);
+            labelResult.Text = "Расшифрованный текст: " + decryptedText;
+        }
+    }
+
+    public static class HillCipher
+    {
+        public static string Encrypt(string text, string key)
+        {
+            text = text.ToUpper();
+            key = key.ToUpper();
+
+            // Проверка на валидность ключа (должна быть квадратной матрицей)
+            int m = (int)Math.Sqrt(key.Length);
+            if (m * m != key.Length)
+            {
+                throw new ArgumentException("Ключ должен быть квадратной матрицей.");
+            }
+
+            // Создание матрицы ключа
+            int[,] keyMatrix = new int[m, m];
+            int k = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    keyMatrix[i, j] = key[k++] - 'A';
+                }
+            }
+
+            // Дополнение текста до длины, кратной размеру матрицы
+            while (text.Length % m != 0)
+            {
+                text += 'X';
+            }
+
+            // Шифрование
+            string ciphertext = "";
+            for (int i = 0; i < text.Length; i += m)
+            {
+                // Преобразование блока текста в вектор
+                int[] vector = new int[m];
+                for (int j = 0; j < m; j++)
+                {
+                    vector[j] = text[i + j] - 'A';
+                }
+
+                // Умножение вектора на матрицу ключа
+                int[] result = new int[m];
+                for (int j = 0; j < m; j++)
+                {
+                    for (int l = 0; l < m; l++)
+                    {
+                        result[j] += keyMatrix[j, l] * vector[l];
+                    }
+                    result[j] %= 26; // Модуль 26 для букв алфавита
+                }
+
+                // Преобразование результата в буквы
+                for (int j = 0; j < m; j++)
+                {
+                    ciphertext += (char)(result[j] + 'A');
+                }
+            }
+
+            return ciphertext;
+        }
+
+        public static string Decrypt(string ciphertext, string key)
+        {
+            ciphertext = ciphertext.ToUpper();
+            key = key.ToUpper();
+
+            // Проверка на валидность ключа
+            int m = (int)Math.Sqrt(key.Length);
+            if (m * m != key.Length)
+            {
+                throw new ArgumentException("Ключ должен быть квадратной матрицей.");
+            }
+
+            // Создание матрицы ключа
+            int[,] keyMatrix = new int[m, m];
+            int k = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    keyMatrix[i, j] = key[k++] - 'A';
+                }
+            }
+
+            // Нахождение обратной матрицы ключа
+            int[,] inverseKeyMatrix = InverseMatrix(keyMatrix);
+
+            // Расшифровка
+            string plaintext = "";
+            for (int i = 0; i < ciphertext.Length; i += m)
+            {
+                // Преобразование блока шифртекста в вектор
+                int[] vector = new int[m];
+                for (int j = 0; j < m; j++)
+                {
+                    vector[j] = ciphertext[i + j] - 'A';
+                }
+
+                // Умножение вектора на обратную матрицу ключа
+                int[] result = new int[m];
+                for (int j = 0; j < m; j++)
+                {
+                    for (int l = 0; l < m; l++)
+                    {
+                        result[j] += inverseKeyMatrix[j, l] * vector[l];
+                    }
+                    result[j] = (result[j] % 26 + 26) % 26; // Модуль 26 для букв алфавита
+                }
+
+                // Преобразование результата в буквы
+                for (int j = 0; j < m; j++)
+                {
+                    plaintext += (char)(result[j] + 'A');
+                }
+            }
+
+            return plaintext;
+        }
+
+        // Метод для нахождения обратной матрицы
+        public static int[,] InverseMatrix(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            if (n != matrix.GetLength(1))
+            {
+                throw new ArgumentException("Матрица должна быть квадратной.");
+            }
+
+            // Создаем расширенную матрицу
+            int[,] extendedMatrix = new int[n, 2 * n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    extendedMatrix[i, j] = matrix[i, j];
+                }
+                extendedMatrix[i, i + n] = 1;
+            }
+
+            // Приводим расширенную матрицу к верхнетреугольному виду
+            for (int i = 0; i < n; i++)
+            {
+                // Поиск ведущего элемента
+                int pivotRow = i;
+                for (int k = i + 1; k < n; k++)
+                {
+                    if (Math.Abs(extendedMatrix[k, i]) > Math.Abs(extendedMatrix[pivotRow, i]))
+                    {
+                        pivotRow = k;
+                    }
+                }
+
+                // Перестановка строк
+                if (pivotRow != i)
+                {
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        int temp = extendedMatrix[i, j];
+                        extendedMatrix[i, j] = extendedMatrix[pivotRow, j];
+                        extendedMatrix[pivotRow, j] = temp;
+                    }
+                }
+
+                // Нормализация ведущего элемента
+                int pivotValue = extendedMatrix[i, i];
+                if (pivotValue != 0)
+                {
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        extendedMatrix[i, j] /= pivotValue;
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Матрица вырожденная. Обратная матрица не существует.");
+                }
+
+                // Обнуление элементов ниже ведущего
+                for (int k = i + 1; k < n; k++)
+                {
+                    int factor = extendedMatrix[k, i];
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        extendedMatrix[k, j] -= factor * extendedMatrix[i, j];
+                    }
+                }
+            }
+
+            // Приводим к единичной матрице
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int k = i - 1; k >= 0; k--)
+                {
+                    int factor = extendedMatrix[k, i];
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        extendedMatrix[k, j] -= factor * extendedMatrix[i, j];
+                    }
+                }
+            }
+
+            // Извлечение обратной матрицы
+            int[,] inverseMatrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    inverseMatrix[i, j] = extendedMatrix[i, j + n];
+                }
+            }
+            
+            return inverseMatrix;
+            
+        }
+    }
+}
+
